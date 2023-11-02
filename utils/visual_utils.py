e957e74285ca80d005fd3d215943419f237c25ec
itingzon.unicef <itingzon.unicef@rlogin02.cm.cluster>
2023-11-02 17:19:17 -0500

    Refactor satellite image visualization

--- utils/visual_utils.py
+++ utils/visual_utils.py
@@ -2,12 +2,21 @@ import os
 import folium
 import geopandas as gpd
 import rasterio as rio
+import data_utils
+
 from rasterio.plot import show
 import matplotlib.pyplot as plt
 from IPython.display import display
 
 
-def map_coords(filename, index, zoom_start=18, max_zoom=20):
+def map_coords(index, category, iso, zoom_start=18, max_zoom=20, name="clean"):
+    data_config = data_utils._load_data_config()    
+    cwd = os.path.dirname(os.getcwd())
+    
+    vector_dir = data_config['data_dir']
+    filename =  f"{iso}_{name}.geojson"
+    filename = os.path.join(cwd, vector_dir, category, name, filename)
+    
     data = gpd.read_file(filename)
     coords = data.iloc[index].geometry.y, data.iloc[index].geometry.x
     map = folium.Map(location=coords, zoom_start=zoom_start, max_zoom=max_zoom)
@@ -23,15 +32,25 @@ def map_coords(filename, index, zoom_start=18, max_zoom=20):
 
 
 def inspect_images(
-    filename,
-    image_dir,
     iso,
+    category,
     n_rows=8,
     n_cols=4,
     index=0,
     figsize=(15, 35),
-    category="SCHOOL",
+    filename=None,
+    name="clean",
 ):
+    data_config = data_utils._load_data_config()
+    cwd = os.path.dirname(os.getcwd())
+    
+    image_dir = data_config['rasters_dir']
+    vector_dir = data_config['data_dir']
+    
+    if not filename:
+        filename =  f"{iso}_{name}.geojson"
+        filename = os.path.join(cwd, vector_dir, category, name, filename)
+    
     data = gpd.read_file(filename)
     fig, axes = plt.subplots(n_rows, n_cols, figsize=figsize)
     samples = data.iloc[index : index + (n_rows * n_cols)]
@@ -39,7 +58,7 @@ def inspect_images(
 
     increment = 1
     for idx, item in samples.iterrows():
-        class_dir = os.path.join(image_dir, f"{iso}/{category.lower()}")
+        class_dir = os.path.join(cwd, image_dir, f"{iso}/{category.lower()}")
         filepath = os.path.join(class_dir, f"{item.UID}.tiff")
 
         image = rio.open(filepath)
