5ef9bd343a12ddcef0360fd4b49b26999e97c8f7
itingzon.unicef <itingzon.unicef@rlogin01.cm.cluster>
2023-11-03 19:34:32 -0500

    Remove load_data_config() and add config to function parameters

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -9,12 +9,9 @@ import numpy as np
 import pandas as pd
 import geopandas as gpd
 
-from tqdm.notebook import tqdm
+from tqdm import tqdm
 from pyproj import Proj, Transformer
 from scipy.sparse.csgraph import connected_components
-
-try: import config_utils
-except: from utils import config_utils
 
 pd.options.mode.chained_assignment = None
 logging.basicConfig(level=logging.INFO)
@@ -40,23 +37,11 @@ def _makedir(out_dir):
     return out_dir
 
 
-def _load_data_config(filename="configs/data_config.yaml"):
-    """Loads the data config, which contains the URL of data sources."""
-
-    cwd = os.path.dirname(os.getcwd())
-    data_config_file = os.path.join(cwd, filename)
-    data_config = config_utils.create_config(data_config_file)
-    return data_config
-
-
-def _get_iso_regions(data, iso_code):
+def _get_iso_regions(config, data, iso_code):
     """Adds country, region, and subregion to dataframe."""
 
     # Load ISO codes of countries and regions/subregions
-    data_config = _load_data_config()
-    url = data_config["iso_regional_codes"]
-    codes = pd.read_csv(url)
-
+    codes = pd.read_csv(config["iso_regional_codes"])
     subcode = codes.query(f"`alpha-3` == '{iso_code}'")
     data["iso"] = iso_code
     data["country"] = subcode["name"].values[0]
@@ -84,15 +69,19 @@ def _convert_to_crs(data, src_crs="EPSG:4326", target_crs="EPSG:3857"):
     return data
 
 
-def _concat_data(data, out_file, verbose=True):
+def _concat_data(data, out_file=None, verbose=True):
     """Concatenates a list of datasets and converts it to a GeoDataFrame."""
 
     data = pd.concat(data).reset_index(drop=True)
     data = gpd.GeoDataFrame(data, geometry=data["geometry"], crs="EPSG:4326")
-    data.to_file(out_file, driver="GeoJSON")
+    
+    if out_file:
+        data.to_file(out_file, driver="GeoJSON")
+        logging.info(f"Generated {out_file}")
+    
     if verbose:
         logging.info(f"Data dimensions: {data.shape}, CRS: {data.crs}")
-        logging.info(f"Generated {out_file}")
+    
     return data
 
 
@@ -107,14 +96,14 @@ def _generate_uid(data, category):
     return data
 
 
-def _prepare_data(data, iso_code, category, source, columns, out_file=None):
+def _prepare_data(config, data, iso_code, category, source, columns, out_file=None):
     """Prepare datasets to have uniform columnns"""
 
     if "name" not in data.columns:
         data["name"] = None
     data["source"] = source.upper()
     data = data.drop_duplicates("geometry", keep="first")
-    data = _get_iso_regions(data, iso_code)
+    data = _get_iso_regions(config, data, iso_code)
     data = _generate_uid(data, category)
     data = data[columns]
     if out_file:
@@ -122,18 +111,22 @@ def _prepare_data(data, iso_code, category, source, columns, out_file=None):
     return data
 
 
-def _get_geoboundaries(iso_code, out_dir="data/geoboundary", adm_level="ADM0"):
+def _get_geoboundaries(config, iso_code, out_dir=None, adm_level="ADM0"):
     """Fetches the geoboundary given an ISO code"""
 
     # Query geoBoundaries
-    out_dir = _makedir(out_dir)
-    data_config = _load_data_config()
+    if not out_dir:
+        cwd = os.path.dirname(os.getcwd())
+        out_dir = os.path.join(cwd, config['rasters_dir'], "geoboundaries")
+    if not os.path.exists(out_dir):
+        out_dir = _makedir(out_dir)
+        
     try:
-        url = f"{data_config['geoboundaries_url']}{iso_code}/{adm_level}/"
+        url = f"{config['geoboundaries_url']}{iso_code}/{adm_level}/"
         r = requests.get(url)
         download_path = r.json()["gjDownloadURL"]
     except:
-        url = f"{data_config['geoboundaries_url']}{iso_code}/ADM0/"
+        url = f"{config['geoboundaries_url']}{iso_code}/ADM0/"
         r = requests.get(url)
         download_path = r.json()["gjDownloadURL"]
 
@@ -151,13 +144,15 @@ def _get_geoboundaries(iso_code, out_dir="data/geoboundary", adm_level="ADM0"):
 
 def _read_data(data_dir, exclude=[]):
     """Reads and concatenates data from a directory of files."""
-    
+
     data_dir = _makedir(data_dir)
     files = next(os.walk(data_dir), (None, None, []))[2]
     files = [file for file in files if file not in exclude]
 
     data = []
-    for file in (pbar := tqdm(files, total=len(files))):
+    bar_format = "{l_bar}{bar:20}{r_bar}{bar:-20b}"
+    pbar = tqdm(files, total=len(files), bar_format=bar_format)
+    for file in pbar:
         pbar.set_description(f"Reading {file}")
         filename = os.path.join(data_dir, file)
         subdata = gpd.read_file(filename)
@@ -169,7 +164,9 @@ def _read_data(data_dir, exclude=[]):
 
 
 def _drop_duplicates(data, priority):
-    data["temp_source"] = pd.Categorical(data["source"], categories=priority, ordered=True)
+    data["temp_source"] = pd.Categorical(
+        data["source"], categories=priority, ordered=True
+    )
     data = data.sort_values("temp_source", ascending=True).drop_duplicates(["group"])
     data = data.reset_index(drop=True)
     return data
@@ -177,7 +174,7 @@ def _drop_duplicates(data, priority):
 
 def _connect_components(data, buffer_size):
     # Dissolve overlapping geometries based on: https://gis.stackexchange.com/a/271737
-    
+
     temp = data.copy()
     if data.crs != "EPSG:3857":
         temp = _convert_to_crs(data, target_crs="EPSG:3857")
