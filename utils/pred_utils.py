57440b67ca9c629c83ca57be6e3f94adc6df5e8a
itingzon.unicef <itingzon.unicef@rlogin01.cm.cluster>
2024-02-26 08:22:22 -0600

    Read tiles file if exists

--- utils/pred_utils.py
+++ utils/pred_utils.py
@@ -373,6 +373,8 @@ def generate_pred_tiles(config, iso_code, spacing, buffer_size, adm_level="ADM2"
     cwd = os.path.dirname(os.getcwd())
     out_dir = data_utils._makedir(os.path.join(cwd, "output", iso_code, "tiles"))
     out_file = os.path.join(out_dir, f"{iso_code}_{shapename}.gpkg")
+    if os.path.exists(out_file):
+        return gpd.read_file(out_file)
     
     points = data_utils._generate_samples(
         config, 
